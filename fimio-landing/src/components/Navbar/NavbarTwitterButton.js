36427a91a84de271880e89ebafc4fa22790f57ca
Daniel Huang <danielhuang@g.ucla.edu>
2022-05-23 12:03:07 -0700

    feat: added twitter button

---
+++ fimio-landing/src/components/Navbar/NavbarTwitterButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavbarTwitterButtonS } from "./NavbarTwitterButtonStyle";
+
+const NavbarTwitterButton = () => {
+	return (
+		<NavbarTwitterButtonS
+			href={`https://twitter.com/fimioxyz`}
+			target='_blank'
+		>
+			Follow
+		</NavbarTwitterButtonS>
+	);
+};
+
+export default NavbarTwitterButton;
---
+++ fimio-landing/src/components/Navbar/NavbarTwitterButtonStyle.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const NavbarTwitterButtonS = styled.a`
+    text-decoration: none;
+    align-self: flex-end;
+    margin-left: 50px;
+    font-style: normal;
+    font-weight: 400;
+    color: #000000;
+    font-size: 18px;
+    line-height: 60px;
+    letter-spacing: 0.04em;
+    cursor: pointer;
+
+    @media screen and (max-width: 600px) {
+        visibility: hidden;
+    }
+`;
+
+export const NavBarTwitterButtonSideBar = styled.a`
+    text-decoration: none;
+    align-self: flex-end;
+    margin-left: 50px;
+    font-style: normal;
+    font-weight: 400;
+    color: #000000;
+    font-size: 18px;
+    line-height: 60px;
+    letter-spacing: 0.04em;
+    cursor: pointer;
+`;
