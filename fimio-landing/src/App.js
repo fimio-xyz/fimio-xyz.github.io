f0327e9a2c40a5590e85569760f1d36eed4c6a26
Daniel Huang <danielhuang@g.ucla.edu>
2022-05-23 12:03:52 -0700

    chore: re-add files from reboot

---
+++ fimio-landing/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NavbarTwitterButton from "./components/Navbar/NavbarTwitterButton";
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarTwitterButton/>
+    </div>
+  );
+}
+
+export default App;
